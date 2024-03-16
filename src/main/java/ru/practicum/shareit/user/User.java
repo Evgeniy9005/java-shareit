2652f0f3b713155f1246e5786a77fbdd81dee9e5
Evgeniy9005 <evgeniy_90_05@mail.ru>
2024-03-16 11:11:52 +1100

    заменена аннотация @Data в User

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,12 +1,15 @@
 package ru.practicum.shareit.user;
 
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @Table(name = "users", schema = "public")
 public class User {
